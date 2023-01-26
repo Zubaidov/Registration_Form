dd66ea7a7081f3f58fab3b14d8b246932e019194
Zubaidov <shohruz.zubaidov@gmail.com>
2023-01-26 15:25:00 +0400

    trying to fix static files

--- registration_form/settings.py
+++ registration_form/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'registration_form.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': str(BASE_DIR / 'db.sqlite3'),
     }
 }
 
@@ -117,13 +117,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'register/static')
-]
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'register/assest')
+STATIC_ROOT = '/home/ubuntu/Registration_Form/register'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    ('assets', '/home/ubuntu/Registration_Form/register/static'),
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
