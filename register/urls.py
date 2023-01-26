556e0f32d69e0c377de5e284e6d554f84ac87638
Zubaidov <shohruz.zubaidov@gmail.com>
2023-01-26 20:06:59 +0400

    new updates

--- register/urls.py
+++ register/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name = 'index'),
+    path('', views.home, name='index'),
+    path('moveto/', views.moveto, name='moveto'),
 ]
--- register/views.py
+++ register/views.py
@@ -1,6 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpResponseRedirect
 
 # Create your views here.
 
+
 def home(request):
     return render(request, 'register/index.html')
+
+
+def moveto(request):
+    f_name = request.GET['cl_fname']
+    l_name = request.GET['cl_lname']
+    e_email = request.GET['cl_email']
+    n_pass = request.GET['cl_pass']
+    c_pass = request.GET['cl_cpass']
+    r_url = 'https://www.instagram.com/zubaydov.sh/'
+    if n_pass == c_pass:
+        return HttpResponseRedirect(r_url)
+    else:
+        return HttpResponseRedirect('http://127.0.0.1:8000/')
